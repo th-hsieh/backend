f748d0a8384cf267d074c5c9a0ac4cd0139a5fc3
Stasy <t.hsuanhsieh@gmail.com>
2023-05-14 13:34:09 +0800

    CRUD

--- src/main/java/org/Stasy/digital_nomad_backend/controller/NotesController.java
+++ src/main/java/org/Stasy/digital_nomad_backend/controller/NotesController.java
@@ -2,6 +2,7 @@ package org.Stasy.digital_nomad_backend.controller;
 
 import org.Stasy.digital_nomad_backend.entity.Note;
 import org.Stasy.digital_nomad_backend.repository.NotesRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,16 @@ public class NotesController {
     @PostMapping("/notes")
     public ResponseEntity<Note> createNote(@RequestBody Note note){
         return new ResponseEntity<Note>(nRepo.save(note),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> readNote(@PathVariable Long id){
+        return new ResponseEntity<Note>(nRepo.findById(id).get(),HttpStatus.OK);
+    }
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id){
+        nRepo.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
 
 }
